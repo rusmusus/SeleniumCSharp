4f9fbaf4974a1115a46b5d443c478c34ec1ba47b
Rustam Ahmedov <rusmusus@gmail.com>
2018-06-02 17:34:56 -0400

    First Commit

---
+++ SeleniumSetup/DemoWebApp/HerokuApp.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using Xunit;
+
+namespace DemoWebApp
+{
+    public class HerokuApp
+    {
+        IWebDriver driver;
+
+        [Fact]
+        public void StartApp()
+        {
+            //driver = new FirefoxDriver();
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/");
+        }
+    }
+}
---
+++ SeleniumSetup/SeleniumSetup/EntryPoint.cs
@@ -0,0 +1,46 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main(string[] args)
+    {
+
+        string URL = "http://testing.todorvachev.com/selectors/id";
+        string ID = "testImage";
+
+        IWebDriver driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+        driver.Navigate().GoToUrl(URL);
+
+        IWebElement element = driver.FindElement(By.Id(ID));
+        if (element.Displayed)
+        {
+            GreenMessage("Yes! I can see the element.");
+        }
+        else
+        {
+            RedMessage("NO! I do NOT see the element!");
+        }
+
+        //Thread.Sleep(3000);
+        //driver.Quit();
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
