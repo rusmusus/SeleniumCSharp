ce642099a357bc39bd9a7fcc6c8c66fac96ba33c
Rustam Ahmedov <rusmusus@gmail.com>
2018-06-10 01:43:08 -0400

    LoginTest method maintained

--- SeleniumSetup/DemoWebApp/HerokuApp.cs
+++ SeleniumSetup/DemoWebApp/HerokuApp.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using System.Threading;
 using Xunit;
 
 namespace DemoWebApp
@@ -16,6 +17,30 @@ namespace DemoWebApp
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/");
+            IWebElement element = driver.FindElement(By.XPath("//a[contains(text(), 'A/B Testing')]"));
+            element.Click();
+            Assert.Equal("http://the-internet.herokuapp.com/abtest", driver.Url);
+            Thread.Sleep(5000);
+            driver.Dispose();
+        }
+
+        [Fact]
+        public void LoginTest()
+        {
+            driver = new FirefoxDriver();
+            //driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/");
+            IWebElement login = driver.FindElement(By.XPath("//a[contains(text(), 'Basic Auth')]"));
+            login.Click();
+
+            driver.SwitchTo().Alert().SendKeys("admin" + Keys.Tab + "admin");
+            driver.SwitchTo().Alert().Accept();
+
+            Thread.Sleep(3000);
+            Assert.Equal("Congratulations! You must have the proper credentials.", driver.FindElement(By.XPath("//*[@id='content']/div/p")).Text);
+            Thread.Sleep(5000);
+            driver.Dispose();
         }
     }
 }
